9e87c4a7d0f83757d0f48cad646549363f903996
予一兮 <2270300912@qq.com>
2018-09-21 15:26:46 +0800

    no message

---
+++ src/main/java/com/onion/axon/axonshop/AxonShopApplication.java
@@ -0,0 +1,12 @@
+package com.onion.axon.axonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonShopApplication.class, args);
+    }
+}
